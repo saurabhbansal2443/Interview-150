edf0fc4165e4461ba6e54a9129e98bb5fad4bfd6
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-03-15 18:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (10.85%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -5,3 +5,6 @@ class Solution {
         return arr[arr.length-1].length(); 
     }
 }
+
+// first we have used trim to remove starting and ending extra space removing 
+// then we split the string then find out the length of last word 
