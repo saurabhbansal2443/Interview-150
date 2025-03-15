25adff8b17f7c10172975a8852cc52acdd0663bf
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-03-15 11:28:34 +0530

    Time: 2 ms (29.86%) | Memory: 57.7 MB (13.84%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,26 +1,31 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        if(nums.length == 0 || nums.length == 1 || k==0 ) return ; 
-        int effectiveRotation =k %  nums.length  ; 
+        if (nums.length == 0 || nums.length == 1 || k == 0)
+            return;
+        int effectiveRotation = k % nums.length;
 
-        reverse(nums, 0 , nums.length-effectiveRotation-1); 
-        reverse(nums, nums.length-effectiveRotation , nums.length-1); 
-        reverse(nums, 0 , nums.length-1); 
-        
+        reverse(nums, 0, nums.length - effectiveRotation - 1);
+        reverse(nums, nums.length - effectiveRotation, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+
     }
-    public void reverse(int[] arr , int start , int end ){
-        if( start >= end ){
-            return ; 
+
+    public void reverse(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
         }
 
-        while(start<end ){
-            int k = arr[start]; 
-            arr[start] = arr[end] ; 
-            arr[end] = k ; 
-            start++; 
-            end--; 
+        while (start < end) {
+            int k = arr[start];
+            arr[start] = arr[end];
+            arr[end] = k;
+            start++;
+            end--;
         }
-
 
     }
 }
+
+// in this question we have used a trick to solve this in o(N) time and o(1) 
+//space is that first we reverse the first part of array then second part 
+// then the whole array this  gives use correct ans 
