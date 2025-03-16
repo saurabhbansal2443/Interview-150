bdb01c6bd90c9ef30165800d94127162e43e970d
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-03-16 08:32:07 +0530

    Time: 29 ms (51.02%) | Memory: 93.6 MB (39.37%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,39 +1,37 @@
 class RandomizedSet {
+
     ArrayList<Integer> arr ; 
-    HashMap<Integer,Integer> map ; 
+    HashMap<Integer,Integer> hm ; 
+
     public RandomizedSet() {
-        arr = new ArrayList<>(); 
-        map = new HashMap<>(); 
+            arr = new ArrayList<>(); 
+            hm = new HashMap<>(); 
     }
     
     public boolean insert(int val) {
-        boolean containsValue = map.containsKey(val); 
-
-        if( containsValue == true  ) return false ; 
+        
+        if(hm.containsKey(val)) return false ; 
 
         arr.add(val); 
-
-        map.put(val , arr.size()-1 ); 
-
+        hm.put(val , arr.size()-1 ); 
         return true ; 
     }
     
     public boolean remove(int val) {
-         boolean containsValue = map.containsKey(val); 
+      boolean containsValue = hm.containsKey(val); 
            if( containsValue == false ) return false ; 
 
-           int valIndex = map.get(val); 
+           int valIndex = hm.get(val); 
            int lastValue =  arr.get(arr.size()-1);
            arr.set(valIndex , lastValue ); 
-           map.put(lastValue  , valIndex); 
+           hm.put(lastValue  , valIndex); 
            arr.remove(arr.size()-1);
-           map.remove(val);
+           hm.remove(val);
 
            return true ; 
-
     }
     
-    public int getRandom() {
+   public int getRandom() {
           if (arr.isEmpty()) throw new IllegalStateException("Set is empty");
             int randomNumber = (int) ((Math.random() * arr.size()));
 
@@ -41,6 +39,10 @@ class RandomizedSet {
     }
 }
 
-// In this code we have used a ArrayList and a hashmap to make this new datastructure  in which insert and delete is of complexity 0(1), 
-//insert is easy if element is present donot add that if it is not add to array and store in map ( val , index ), we are storing this so that we know where is element and this will help us to remove this easily in o(1); 
-// delete - in delete first check element is present or not if present then get the index of that element and then put the last element of array at its position, and also chnage the index of the last element in map and then remove the last element 
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
