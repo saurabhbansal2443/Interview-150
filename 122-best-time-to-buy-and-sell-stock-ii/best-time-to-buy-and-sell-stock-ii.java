b3c35310019973bc22f383d9089169bdc383516b
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-03-17 09:59:55 +0530

    Time: 1 ms (76.75%) | Memory: 45.9 MB (44.82%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -10,7 +10,13 @@ class Solution {
         return profit ; 
     }
 }
-
-// int this question we have used the trick that if adjacent value that means 
-// prices[i] - prices[i-1] is giving positive value only adding that 
-// transcition to that profit 
+// in this question in order to solve this make a graph of prices then you will see there 
+// that there is some slopes which is going up and some slopes which are going down 
+// from this slopes we have got a understanding that when slopes are going upwards 
+// that means stock price is increasing and we have to calculate the profit and if 
+// buy the stock on that day and sell on the next day it is goin to give us profit 
+// so here in this question as we have to calculate the maximum profit of the series of the 
+// day so will take the profit and will ignore the losses so 
+// in this question we have used the trick that if adjacent value that means 
+// prices[i] - prices[i-1] is giving positive value (that means profit ) only adding that 
+// transcition to that profit and returning the maximum profit 
