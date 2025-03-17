4a9761112a0a93468fc9f96c5a80816400974902
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-03-17 09:52:17 +0530

    Time: 2 ms (75.04%) | Memory: 53.4 MB (29.10%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -18,4 +18,12 @@ class Solution {
 
         return ans ; 
     }
+
+    // In this question we have to find a majority element that comes n/2 or 
+    // more than this so we have used a approach in which we assumed the first 
+    // element to be that majority and incresing when same element we get but 
+    // decresing when the diffrent element we get and if the element frequency goes to zero 
+    // that means that it is not majority element and we have taken the elemnet at 
+    // which the frequency has become zero, as the majority elemnt is at least more than 
+    // n/2 time it will cancel other elemnts and becomes the majority one 
 }
