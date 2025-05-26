8f9daf9f6dda5c73b4bc93e5ff7edb58bd2c2056
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-05-26 23:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (8.94%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,27 +1,21 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
-        int p1 = 0 ; 
-        int p2 = 0 ; 
+        if (nums.length == 0)
+            return 0;
 
-        while(p2< nums.length ){
-            
-            if(nums[p2] == val ) p2++ ; 
-            else {
-                int k = nums[p1]; 
-                nums[p1] = nums[p2]; 
-                nums[p2] = k ; 
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p2 < nums.length) {
+
+            if (nums[p2] != val) {
+                nums[p1] = nums[p2];
                 p1++;
-                p2++;
             }
+            p2++;
         }
 
-        return p1; 
-    }
+        return p1;
 
-    // in this question we have to replace the val from the array, so we have 
-    // taken 2 pointer, remove in such a way that after removing all the 
-    //non(val) value should appear at the first part of the array, so we have 
-    //taken 2 pointer such that will exchaneg the value of ptr when p1 is at 
-    //val and p2 is non val after that return p1 the number of non val value 
+    }
 }
