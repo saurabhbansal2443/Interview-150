9bc8eb4d995bf14c77d2497dadc75881f55d319b
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2025-12-28 18:26:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (5.84%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,18 +1,20 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-       int start = 0 ; 
-       int end = 0 ; 
-       while(end<nums.length){
-        if(nums[end] == val){
-            end++; 
-        }else {
-            int k = nums[start]; 
-            nums[start] = nums[end]; 
-            nums[end] = k ; 
-            start++; 
-            end++; 
+        int start = 0 ; 
+        int end = 0 ; 
+
+        while(end< nums.length){
+            
+            if(nums[end] == val ){
+                end++; 
+            }else{
+                int k = nums[start]; 
+                nums[start] = nums[end]; 
+                nums[end] = k ; 
+                start++; 
+                end++; 
+            }
         }
-       }
-       return start;  
+        return start ; 
     }
 }
