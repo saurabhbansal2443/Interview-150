8f9777a5edbdafcfbb31c9e700b92b55b93724e6
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-01-01 15:00:25 +0530

    Time: 1 ms (99.83%) | Memory: 55.7 MB (29.96%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,29 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int ans = nums[0]; 
-        int count = 1 ; 
+       int ans = nums[0]; 
+       int count = 1 ; 
 
-        for(int i=1 ; i<nums.length ; i++ ){
-            if( ans == nums[i]){
-                count +=1 ; 
-            }else{
-                count -=1 ; 
-            }
+       for(int i=1 ; i<nums.length ; i++ ){
+        int ele = nums[i]; 
 
-            if(count == 0 ){
-                ans = nums[i]; 
-                count =1 ; 
-            }
+        if(ele == ans ){
+            count++; 
+        }else{
+            count--; 
         }
+            if(count ==0 ){
+                ans = ele ; 
+                count = 1 ; 
+            }
+       } 
 
-        return ans ; 
+       return ans ; 
     }
-
-    // In this question we have to find a majority element that comes n/2 or 
-    // more than this so we have used a approach in which we assumed the first 
-    // element to be that majority and incresing when same element we get but 
-    // decresing when the diffrent element we get and if the element frequency goes to zero 
-    // that means that it is not majority element and we have taken the elemnet at 
-    // which the frequency has become zero, as the majority elemnt is at least more than 
-    // n/2 time it will cancel other elemnts and becomes the majority one 
 }
