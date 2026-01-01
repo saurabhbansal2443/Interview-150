8a5fcb32104f889e42d2cfe15504eb488f08e04f
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-01-01 10:49:06 +0530

    Time: 1 ms (73.97%) | Memory: 46.8 MB (20.58%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,28 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
-    if ( nums.length == 1) return 1 ; 
+        if (nums.length == 1)
+            return 1;
+        int start = 0;
+        int end = 1;
 
-       int p1 = 1 ; 
-       int p2 = 1 ; 
-
-       while(p2< nums.length ){
-
-            if(nums[p2] == nums[p2-1]){
-                p2++ ; 
-            }else{
-                nums[p1] = nums[p2]; 
-                p1++ ; 
-                p2++ ; 
+        while (end < nums.length) {
+            if (nums[start] == nums[end]) {
+                end++; 
+            }else {
+                nums[start+1] = nums[end]; 
+                start++; 
+                end++; 
             }
-       }
-
-       return p1 ; 
+        }
+        return start+1; 
     }
 }
-// we have to remove the duplicates from a sorted array(now in sorted array the
-// dupliactes number comes adjancetly ) so we have taken 2 pointer placed the 
-// first pointer on 1 index and check the previous number if they are same that 
-// means they are duplicate hence we remove to the number so we just move ptr2 
-// so that it can go on some other unique number and then we will replace that 
-// number with p1 
