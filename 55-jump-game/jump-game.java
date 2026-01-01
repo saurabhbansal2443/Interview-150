f5be77588ba65b785f1a7aa3a2821b61c99fc919
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-01-01 19:18:27 +0530

    Time: 3 ms (41.93%) | Memory: 47.8 MB (60.16%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,23 +1,29 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int jump = nums[0]; 
 
-        for(int i=1 ; i<nums.length ; i++ ){
-            if( i >  jump ){
-                return false ; 
-            }else if ( jump >= nums.length-1){
-                return true ; 
-            }
-            jump = Math.max(i+nums[i] , jump ); 
+        int length = nums.length;
+
+        int maxAllowedIndex = nums[0];
+
+        if (maxAllowedIndex == length - 1) {
+            return true;
+        }
+        if(nums[0] == 0 ){
+            return false ; 
+        }else if (nums.length == 1){
+            return true ; 
         }
 
-        return true ; 
-    }
+        int idx = 1;
 
-    // in this question we have used a logic we are calculating the highest 
-    // number of index we can reach so we are adding the index and value if 
-    // at any index we can reach the maximum value that means last index 
-    // we will return true and if there is any index which is not possible 
-    // returining false from there and jump is storing the maximum number of 
-    // index can be reached 
+        while(idx <= maxAllowedIndex && idx < length) {
+           maxAllowedIndex = Math.max(maxAllowedIndex,nums[idx] + idx); 
+
+           if(nums[idx] + idx >= length-1){
+            return true ; 
+           } 
+           idx++;
+        }
+        return false ; 
+    }
 }
