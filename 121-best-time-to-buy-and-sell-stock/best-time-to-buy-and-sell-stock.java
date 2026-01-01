a71c988624533a83b4f0de4968f57040de5a7619
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-01-01 15:30:47 +0530

    Time: 2 ms (42.34%) | Memory: 89 MB (97.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,26 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[] max = new int[n];
+       int[] max = new int[prices.length]; 
 
-        max[n - 1] = prices[n - 1];
+       max[prices.length-1] = 0 ; 
 
-        for (int i = n - 2; i >= 0; i--) {
+       for(int i=prices.length-2; i>=0 ; i--){
+            max[i] = Math.max(max[i+1],prices[i+1]); 
+       } 
 
-            max[i] = Math.max(max[i + 1], prices[i + 1]);
-        }
+       int profit = 0 ; 
 
-        int profit = 0;
+       for(int i=0 ; i<prices.length ; i++){
+         int currProfit = max[i] - prices[i]; 
 
-        for (int i = 0; i < n; i++) {
-            profit = Math.max(profit, max[i] - prices[i]);
-        }
-        return profit;
+         if(currProfit>profit){
+            profit = currProfit; 
+         }
+       } 
+       return profit>0?profit : 0 ; 
     }
-
-    // we have taken a approach where first we have find the max array which is 
-    // just a array containing the max value present after that value in the array, 
-    // after that value and then subracting the value and get the maximum profit of that 
-    // day ( means if we buy the stock on that day this is going to be the maximum profit)
-    //from there and  then runned loop over the main array and get the maximum profit 
 }
