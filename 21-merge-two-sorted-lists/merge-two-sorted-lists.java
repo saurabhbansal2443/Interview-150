44268f21ab28fb8e6b8bab30cf77fdd29c16ad25
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-04-25 14:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (39.99%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,38 +1,41 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null)
+        if (list1 == null) {
             return list2;
-        if (list2 == null)
+        } else if (list2 == null) {
             return list1;
+        }
 
         ListNode dummy = new ListNode(-1);
-        ListNode ans = dummy;
 
-        ListNode p1 = list1;
-        ListNode p2 = list2;
+        ListNode ans = dummy ; 
 
-        while (p1 != null || p2 != null) {
-            int val1 = p1 != null ? p1.val : Integer.MAX_VALUE;
-            int val2 = p2 != null ? p2.val : Integer.MAX_VALUE;
-
-            if (val1 < val2) {
-                dummy.next = new ListNode(val1);
-                p1 = p1.next;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                dummy.next = list1;
+                dummy = dummy.next;
+                list1 = list1.next;
             } else {
-                dummy.next = new ListNode(val2);
-                p2 = p2.next;
+                dummy.next = list2;
+                dummy = dummy.next;
+                list2 = list2.next;
             }
-            dummy = dummy.next ; 
+        }
+
+        if(list1 == null ){
+            dummy.next = list2 ; 
+        }else {
+            dummy.next = list1 ; 
         }
 
         return ans.next ; 
