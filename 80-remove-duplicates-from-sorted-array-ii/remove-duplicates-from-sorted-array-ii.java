4e0739a52ae393184b6a2ad27a8f4ebb5c9d5c68
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-07-28 08:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (84.60%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,24 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length ==1 || nums.length ==2 ) return nums.length ; 
-         
-         int p1 =2 ; 
-         int p2 = 2 ; 
+        int start = 2 ; 
+        int end = 2 ; 
 
-         while( p2< nums.length ){
+        while(end < nums.length ){
 
-            if(nums[p1-2]!=nums[p2]){
-                nums[p1] = nums[p2];
-                p1++ ; 
+            if(nums[start -2 ] == nums[end]){
+                end++ ; 
+            }else{
+                nums[start] = nums[end]; 
+                start++; 
+                end++ ; 
             }
-            p2++; 
-         }
-         return p1 ; 
-    }
+        }
 
-    // in this we have to remove duplicates in sorted array but at most 2 times
-    // a element can come at most twice so we are cheking i-2th element is it 
-    //same that means the element is coming 3 times or not and then replacing p1 with 
-    // p2 only so that maximum of 2 times a element can come 
+        return start ; 
+    }
 }
-    
