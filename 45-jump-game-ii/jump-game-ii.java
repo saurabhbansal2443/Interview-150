26fab9468f5eed0e5ceaa823929f789e596c0710
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-07-29 11:43:08 +0530

    Time: 2 ms (30.04%) | Memory: 47.4 MB (21.67%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,35 +1,28 @@
 class Solution {
     public int jump(int[] nums) {
-        // if(nums[0] == 0 ) return 0 ; 
+        
         if(nums.length == 1 ) return 0 ; 
-        int ans =1 ; 
-        int jump = nums[0] ;
-        int index = 0 ;  
+        int jump = 1 ; 
+        int jumpIndex = nums[0];
+        int maxJump = 0 ; 
 
-        while( index < nums.length  ){
+        int index = 0 ; 
 
-            if(jump>= nums.length -1 ){
-                return ans ; 
+        while(index <= nums.length-1 && jumpIndex <nums.length-1 ){
+
+            for(int i = index ; i<= jumpIndex && i <nums.length ; i++ ){
+                maxJump = Math.max(maxJump , (nums[i] + i )); 
             }
-            int maxJump = 0 ; 
-            for(int i=index+1; i <=jump ; i++ ){
-                
-                maxJump= Math.max( maxJump , i+nums[i]); 
-            }
-            ans++; 
-            index = jump ; 
-            jump = maxJump ; 
+            index = jumpIndex; 
+            jump++ ; 
+            jumpIndex  = maxJump ; 
+
         }
 
-        return ans ; 
+        return jump ; 
+
+
+
+        
     }
 }
-
-// in this question we have 2 find the minimum number of jumps to cross the 
-//array or reach the last index for that we have selected a window from index
-// 1 --> nums[0] first then find out the maximum number of index which can be
-// reached from this array and then the next window is nums[0]-->nums[maxJump]
-//kind of this thing 
-
-// if we just see the code it seem to be O(n^2) but the time complexity is just o(n) 
-// as we are traversing the array only one time 
