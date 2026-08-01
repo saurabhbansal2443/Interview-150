bdabfd66b4de7b9cbd3ab61024d961683de90945
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-08-01 08:59:43 +0530

    Time: 2 ms (99.27%) | Memory: 119.3 MB (65.82%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,28 +1,38 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0, totalCost = 0;
-        for (int i = 0; i < gas.length; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
+        // Firstly in this case we are checking the totalGasRequired and totalGasAvaialble is sufficinet or not 
+        int totalGasRequired = 0 ; 
+        int totalGasAvailable = 0 ; 
+
+        int n = gas.length ; 
+
+        for(int i=0 ; i<n ; i++ ){
+
+            totalGasRequired += cost[i]; 
+            totalGasAvailable += gas[i]; 
         }
 
-        if (totalGas < totalCost) {
-            return -1;
+        if(totalGasRequired>totalGasAvailable){
+            return -1 ; 
         }
 
-        int currentGas = 0;
-        int start = 0;
 
-        for (int i = 0; i < gas.length; i++) {
-            currentGas += gas[i] - cost[i];
-            if (currentGas < 0) {
-                currentGas = 0;
-                start = i + 1;
+        int gase = 0 ; 
+        int startingPoint = -1 ; 
+
+        for(int i=0 ; i<n ; i++ ){
+
+            if(cost[i]<= gas[i] && startingPoint == -1 ){
+                gase = gas[i] - cost[i];
+                startingPoint = i ;  
+            }else if ( startingPoint != -1 ){
+                gase += gas[i] - cost[i]; 
+                if(gase<0 ){
+                   startingPoint = -1 ;  
+                }
             }
         }
 
-        return start;        
+        return startingPoint; 
     }
 }
-
-// in this question we have 2 array cost and gas where we have to complete 1 circle for that we have firstly check weather we  the totalcost > totalgas that means it is impossible to to complete circle, so we return -1 and after that if code comes that means that there is a solution, then we are checking for the solution by checking the correct point where to start 
