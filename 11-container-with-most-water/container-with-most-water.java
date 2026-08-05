442e26b0ac0138db2b7590ce4b39b79551f71aa8
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2026-08-05 14:30:49 +0530

    Time: 5 ms (82.96%) | Memory: 77.6 MB (12.78%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,33 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
-        int ans = 0 ; 
+        
         int start = 0 ; 
-        int end = height.length -1 ; 
+        int end = height.length-1 ; 
 
+        int maxWater = 0 ; 
 
-        while ( start < end ){
+        while(start<end){
+
             int width = end - start ; 
-            int tank = Math.min ( height[start] , height[end]); 
-            ans = Math.max( width*tank , ans); 
+            int h = Math.min(height[start], height[end]); 
 
-            if(height[start] < height[end]){
+            maxWater = Math.max(maxWater , h * width ); 
+
+            if(h == height[start]){
                 start++; 
             }else{
                 end--; 
             }
         }
-
-        return ans ; 
-
+        return maxWater ; 
     }
-
-    // in this question we have taken approch of greedy in this question the array is given the 
-    // height of may be container we have to find the maximum water which can be stored between 
-    // the walls of container, so there is very simple concept we want two things to be maximize 
-    // first is width and height so we are using 2 pointer over here start at 0 and end at last 
-    // index and then find the water between these walls as the width is fixed b/w these points 
-    // but the height should be taken minimum of them so we have stored that result and comapre 
-    // with our maximum and now in order to maximize the water we have to leave the smaller wall 
-    // and should contniue with large wall so what ever pointer is on small wall, we will move 
-    // that pointer in order of finding the conatiner with most water 
 }
